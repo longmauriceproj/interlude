18d4187bc61d68f36ead2e6c0dc49196ecebcd19
longmauriceproj <longmaurice255@gmail.com>
2023-06-19 09:02:18 -0700

    created header and footer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,27 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
+import { Footer } from "~/components/Footer";
+import { Header } from "~/components/Header";
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        </div>
+      </div>
+      <div className="relative">
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
     </SessionProvider>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 } satisfies Config;
